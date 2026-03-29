6e171c884fb0ad8c60eebbd0da272c7c2ce9ea0c
J.Bobo <joshuaoduoma@gmail.com>
2026-03-29 18:55:24 +0100

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import "./global.css";
+import { Text, View } from 'react-native'
+
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="text-red-300">Home is where family is</Text>
+    </View>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // Make sure "./App.{js,jsx,ts,tsx}" is in this list!
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
